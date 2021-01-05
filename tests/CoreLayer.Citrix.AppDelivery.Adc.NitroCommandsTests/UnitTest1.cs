062480e6657de2a5fa7fcdb5a3553756a26e07b8
Jan Tytgat <jan.tytgat@corelayer.eu>
2021-01-05 20:17:22 +0100

    Additionql Test projects

---
+++ tests/CoreLayer.Citrix.AppDelivery.Adc.NitroCommandsTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CoreLayer.Citrix.AppDelivery.Adc.NitroCommandsTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
